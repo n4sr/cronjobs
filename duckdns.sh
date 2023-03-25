c5fbd3430979ac5018e202068ee14cb7abb83518
n4sr <54733427+n4sr@users.noreply.github.com>
2023-03-25 17:53:53 -0500

    Added comments to duckdns.sh

--- duckdns.sh
+++ duckdns.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Run the command to update domains registered with DuckDNS.
+# Create files `ducktoken` which contains your API token, and `duckdomains` which
+# contains your subdomains on new lines.
 for domain in $(<duckdomains); do
    curl "https://www.duckdns.org/update?domains=$domain&token=$(<ducktoken)&ip=&ipv6="
 done
