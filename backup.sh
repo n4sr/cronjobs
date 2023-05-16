cb6ba61fb374df31fabe3b8123715e2e3d671cbd
n4sr <54733427+n4sr@users.noreply.github.com>
2023-05-16 08:12:14 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/bash
-# This script archives a specified directory
-# Example: backup.sh <directory to backup> <backup to directory> <number of backups to keep>
-backup_target_dir=$1
-backup_dir=$2
+# Simple Backup Script
+# usage: ./backup.sh <source> <destination> <number of backups to keep>
+
+src=$1
+dest=$2
 number_of_backups=$3
 
 # Check that variables are populated before continuing
 # TODO: add checks for if the path to backup exists. potentially require absolute paths.
-if [[ -z $backup_target_dir ]] || [[ -z $backup_dir ]] || [[ $number_of_backups -le 0 ]]; then
-    echo "something was misconfigured.. check shell script and try again"; exit 1
+if [[ -z $src ]] || [[ -z $dest ]] || [[ $number_of_backups -le 0 ]]; then
+    echo 'usage: ./backup.sh <source> <destination> <number of backups to keep>'
+    exit 1
 fi
 
 # Create the new backup
-new_backup="$backup_dir/$(date +%F_%H.%M.%S.tar.gz)"
-cd $backup_target_dir && tar -caf $new_backup *
+new_backup="$dest/$(date +%F_%H.%M.%S.tar.gz)"
+cd $src && tar -caf $new_backup *
 
 # If the number of backups exceeds the limit, then remove the oldest file based on name.
 # TODO: sort files based on created/modified time so the file naming convention can be more customizable
-backups_to_remove=$(ls $backup_dir | head -n -$number_of_backups)
-cd $backup_dir && if [[ -n $backups_to_remove ]]; then rm $backups_to_remove; fi
+backups_to_remove=$(ls $dest | grep '.tar.gz$' | head -n -$number_of_backups)
+cd $dest && if [[ -n $backups_to_remove ]]; then rm $backups_to_remove; fi
