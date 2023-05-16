9c43c676da6cb0e67693c6f742eec90b637ce25b
n4sr <54733427+n4sr@users.noreply.github.com>
2023-05-16 10:35:51 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/bash
-# Simple Backup Script
+# Backup cronjob
 # usage: ./backup.sh <source> <destination> <number of backups to keep>
 
-src=$1
-dest=$2
-number_of_backups=$3
-
 # Check that variables are populated before continuing
-# TODO: add checks for if the path to backup exists. potentially require absolute paths.
-if [[ -z $src ]] || [[ -z $dest ]] || [[ $number_of_backups -le 0 ]]; then
+if [[ -z $1 ]] || [[ -z $2 ]] || [[ $3 -le 0 ]]; then
     echo 'usage: ./backup.sh <source> <destination> <number of backups to keep>'
     exit 1
 fi
 
-# Create the new backup
-new_backup="$dest/$(date +%F_%H.%M.%S.tar.gz)"
-cd $src && tar -caf $new_backup *
+dest="$2/$(basename $1)~$(date +%F_%R:%S).tar.gz"
+
+cd $1 && tar -caf $dest *
 
 # If the number of backups exceeds the limit, then remove the oldest file based on name.
-# TODO: sort files based on created/modified time so the file naming convention can be more customizable
-backups_to_remove=$(ls $dest | grep '.tar.gz$' | head -n -$number_of_backups)
-cd $dest && if [[ -n $backups_to_remove ]]; then rm $backups_to_remove; fi
+backups_to_remove=$(ls $2 | grep -E "$(basename $1).*\.tar\.gz$" | head -n -$3)
+cd $2 && if [[ -n $backups_to_remove ]]; then rm $backups_to_remove; fi
