8a6496f40ee08ab924ce05f70262d6d6d662aac7
n4sr <54733427+n4sr@users.noreply.github.com>
2023-05-16 08:21:50 -0500

    Update njalla.sh

--- njalla.sh
+++ njalla.sh
@@ -4,7 +4,7 @@
 # input file ($1) should have a domain name and key pair seperated by a colon (:) on each line.
 # logfile is optional
 
-if [[ -n $1 ]]; then
+if [[ -z $1 ]]; then
     echo 'usage: ./njalla.sh <infile> <logfile>'
 fi
 
@@ -12,11 +12,6 @@ fi
 # TODO: add more providers to choose from.
 ipv4=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '"')
 ipv6=$(dig -6 TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '"')
-
-if [[ -n $1 ]]; then
-    echo 'usage: ./njalla.sh <infile> <logfile>'
-fi
-
 
 while read line || [[ -n $p ]]; do
     domain=$(echo $line | cut  -d ':' -f 1)
